c0a96bbebbdbf0b8e99bc660ddac4980514c6583
Daniel Gualán <daniel@credia.io>
2026-05-09 21:37:50 -0500

    fix(api): reemplaza bcrypt nativo por bcryptjs

--- packages/api/prisma/seed.ts
+++ packages/api/prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient, Role, UserStatus, BusinessType, SourceType, RiskLevel } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
